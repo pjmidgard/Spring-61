a3afda72d80a0ce35d47a746d77e75764f9730f1
Java Software <pj.midgard@yahoo.com>
2021-11-26 15:25:25 +0000

    Add files via upload

--- Spring-61.py
+++ Spring-61.py
@@ -1012,8 +1012,8 @@ class compression:
                                     #print(szxw3)
                                     
                                     lenf=len(szxw3)
-                                    print(lenf)
-                                    print(blockD)
+                                    #print(lenf)
+                                    #print(blockD)
                                    
                                     if lenf>blockD:
                                             wer="01111111"+sda
